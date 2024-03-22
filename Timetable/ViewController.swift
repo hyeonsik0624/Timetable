74d69b718c934c69d0b887f479af37c3ccf98ae5
Hyeonsik <jhs062424@naver.com>
2024-03-23 08:45:54 +0900

    Initial Commit

---
+++ Timetable/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Timetable
+//
+//  Created by 진현식 on 3/23/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
