d94e4dc318bf8ee73de99f359970d181b6fe5f4a
Hyeonsik <jhs062424@naver.com>
2024-05-03 22:57:48 +0900

    Update Widget UI

--- TimetableWidget/TimetableWidget.swift
+++ TimetableWidget/TimetableWidget.swift
@@ -12,38 +12,26 @@ let appGroupUserDefaults = UserDefaults(suiteName: "group.dev.hyeonsik.timetable
 
 struct Provider: TimelineProvider {
     func placeholder(in context: Context) -> TimetableEntry {
-        let subjects = ["국", "영", "수", "사", "과", "국", "영"]
-        let classrooms = ["1", "2", "3", "4", "5", "6", "7"]
-        
-        return TimetableEntry(date: Date(), subjects: subjects, classrooms: classrooms)
+        let weekDay = getWeekDayNumber()
+        return TimetableEntry(date: Date(), subjects: getSavedSubjects(weekDay), classrooms: getSavedClassrooms(weekDay))
     }
 
     func getSnapshot(in context: Context, completion: @escaping (TimetableEntry) -> ()) {
-        let subjects = ["국", "영", "수", "사", "과", "국", "영"]
-        let classrooms = ["1", "2", "3", "4", "5", "6", "7"]
-        
-        let entry = TimetableEntry(date: Date(), subjects: subjects, classrooms: classrooms)
+        let weekDay = getWeekDayNumber()
+        let entry = TimetableEntry(date: Date(), subjects: getSavedSubjects(weekDay), classrooms: getSavedClassrooms(weekDay))
         completion(entry)
     }
 
     func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
         var entries: [TimetableEntry] = []
-
+        
         let currentDate = Date()
+        let weekDay = getWeekDayNumber()
         
-        var weekDay = Calendar.current.component(.weekday, from: currentDate) - 1
-        
-        if weekDay == 6 || weekDay == 7 {
-            weekDay = 1
-        }
-        
-        let subjects = appGroupUserDefaults?.array(forKey: "subjects:\(weekDay)") as? [String] ?? []
-        let classrooms = appGroupUserDefaults?.array(forKey: "classrooms:\(weekDay)") as? [String] ?? []
-        
-        entries.append(TimetableEntry(date: currentDate, subjects: subjects, classrooms: classrooms))
+        entries.append(TimetableEntry(date: currentDate, subjects: getSavedSubjects(weekDay), classrooms: getSavedClassrooms(weekDay)))
         
         var nextUpdateDate = Calendar.current.startOfDay(for: currentDate)
-        nextUpdateDate = Calendar.current.date(bySettingHour: 3, minute: 0, second: 0, of: nextUpdateDate)!
+        nextUpdateDate = Calendar.current.date(bySettingHour: 1, minute: 0, second: 0, of: nextUpdateDate)!
 
         if nextUpdateDate <= currentDate {
             nextUpdateDate = Calendar.current.date(byAdding: .day, value: 1, to: nextUpdateDate)!
@@ -129,9 +117,6 @@ struct ClassroomTextView: View {
         VStack {
             ForEach(classrooms, id: \.self) { classroom in
                 Text(classroom)
-                    .fontWeight(.medium)
-                    .fontDesign(.rounded)
-                    .lineLimit(1)
             }
         }
     }
@@ -143,17 +128,39 @@ struct TimetableGrid: View {
     var subjects: [String]
     var classrooms: [String]
     
+    var filterdSubjects: [String] {
+        var subjects = subjects
+        
+        if subjects.last == "" {
+            subjects.removeLast()
+        }
+        
+        return subjects
+    }
+    
+    var filterdClassrooms: [String] {
+        var classrooms = classrooms
+        
+        if classrooms.last == "" {
+            classrooms.removeLast()
+        }
+        
+        return classrooms
+    }
+    
     var body: some View {
-        HStack(alignment: .center, spacing: 4) {
-            PeriodGrid(numberOfPeriod: subjects.count)
-            SubjectGrid(subjects: subjects)
-            ClassroomGrid(classrooms: classrooms)
+        HStack(alignment: .center) {
+            PeriodGrid(numberOfPeriod: filterdClassrooms.count)
+            SubjectGrid(subjects: filterdSubjects)
+            ClassroomGrid(classrooms: filterdClassrooms)
         }
     }
 }
 
 struct PeriodGrid: View {
-    let rows: [GridItem] = Array(repeating: GridItem(.flexible()), count: 7)
+    var rows: [GridItem] {
+        Array(repeating: GridItem(.flexible()), count: self.numberOfPeriod)
+    }
     
     let numberOfPeriod: Int
     
@@ -169,7 +176,9 @@ struct PeriodGrid: View {
 }
 
 struct SubjectGrid: View {
-    let rows: [GridItem] = Array(repeating: GridItem(.flexible()), count: 7)
+    var rows: [GridItem] {
+        Array(repeating: GridItem(.flexible()), count: self.subjects.count)
+    }
     
     let subjects: [String]
     
@@ -185,7 +194,9 @@ struct SubjectGrid: View {
 }
 
 struct ClassroomGrid: View {
-    let rows: [GridItem] = Array(repeating: GridItem(.flexible()), count: 7)
+    var rows: [GridItem] {
+        Array(repeating: GridItem(.flexible()), count: self.classrooms.count)
+    }
     
     let classrooms: [String]
     
@@ -201,11 +212,33 @@ struct ClassroomGrid: View {
     }
 }
 
+// MARK: - Helpers
+
+func getSavedSubjects(_ weekDay: Int) -> [String] {
+    return appGroupUserDefaults?.array(forKey: "subjects:\(weekDay)") as? [String] ?? []
+}
+
+func getSavedClassrooms(_ weekDay: Int) -> [String] {
+    return appGroupUserDefaults?.array(forKey: "classrooms:\(weekDay)") as? [String] ?? []
+}
+
+func getWeekDayNumber() -> Int {
+    let currentDate = Date()
+    
+    var weekDay = Calendar.current.component(.weekday, from: currentDate) - 1
+    
+    if weekDay == 6 || weekDay == 7 {
+        weekDay = 1
+    }
+    
+    return weekDay
+}
+
 #Preview(as: .systemSmall) {
     TimetableWidget()
 } timeline: {
-    let subjects = ["국", "영", "수", "사", "과", "국", "영"]
-    let classrooms = ["1", "2", "3", "4", "5", "6", "7"]
+    let subjects = ["체육", "지구과학", "지구과학", "심화국어", "영독작", "동사", ""]
+    let classrooms = ["", "3-4", "3-5", "3-5", "3-4", "가가교", ""]
     
     TimetableEntry(date: Date(), subjects: subjects, classrooms: classrooms)
 }
